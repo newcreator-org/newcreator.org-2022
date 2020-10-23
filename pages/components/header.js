d7640f438e45e176a613f437d039cb5ec7a30fee
yosh1 <yoppy1125yoppy@gmail.com>
2020-10-23 17:24:42 +0900

    Add header / hero

---
+++ pages/components/header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header>
+            <nav className="header__nav">
+                <a href="/">
+                    <h1 className="header__logo">特定非営利活動法人ニュークリエイター・オルグ</h1>
+                </a>
+            </nav>
+        </header>
+    )
+}
