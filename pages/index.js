6899c0ae6406253a5caff19b27e9447a60d721ce
yosh1 <yoppy1125yoppy@gmail.com>
2020-10-24 01:45:46 +0900

    Update font / footer

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Header from './components/header';
 import Hero from './components/hero';
 import Philosophy from './components/philosophy';
 import Board from './components/board';
+import Footer from './components/footer';
+import Font from './components/font';
 
 export default function Home() {
     return (
@@ -12,7 +14,9 @@ export default function Home() {
                 <Hero />
                 <Philosophy />
                 <Board />
+                <Font />
             </main>
+            <Footer />
         </>
     )
 }
