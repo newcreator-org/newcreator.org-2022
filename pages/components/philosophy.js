451099654707bb3db96443d5419b1481c1f7d509
yosh1 <yoppy1125yoppy@gmail.com>
2020-10-23 17:38:02 +0900

    Add board

---
+++ pages/components/philosophy.js
@@ -0,0 +1,36 @@
+export default function Philosophy() {
+    return (
+        <section className="text-gray-700 body-font overflow-hidden">
+
+            <div className="container px-5 py-24 mx-auto">
+                <div className="flex flex-col text-center w-full mb-20">
+                    <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">PHILOSOPHY</h2>
+                    <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">基本情報・理念</h1>
+                </div>
+                <div className="flex flex-wrap -m-12 justify-center">
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-center">
+                        2019年1月に発足した次世代の学びをオープンに制作・提供するNPO法人です。<br />
+                            誰でも自由に使用や改変を行うことができ、無料で使える「オープンソース」という形で提供される教材の<br />開発およびそれらを利用したイベントの開催を行っています。</p>
+                    <div className="p-12 md:w-full xl:w-1/2 flex flex-col items-center text-center">
+                        <span
+                            className="inline-block py-1 px-3 rounded bg-indigo-100 text-indigo-500 text-sm font-medium tracking-widest">VISION</span>
+                        <h2 className="text-xl title-font font-medium text-gray-900 mt-4 mb-4">
+                            IT教育を全国の子どもたちに<br />格差なく届ける
+              </h2>
+                        <p className="leading-relaxed mb-8 message-english">Delivering IT education to
+                all children without disparity</p>
+                    </div>
+                    <div className="p-12 md:w-full xl:w-1/2 flex flex-col items-center text-center">
+                        <span
+                            className="inline-block py-1 px-3 rounded bg-indigo-100 text-indigo-500 text-sm font-medium tracking-widest">MISSION</span>
+                        <h2 className="text-xl title-font font-medium text-gray-900 mt-4 mb-4">
+                            オープンソース教材の提供を通して<br />
+                                            教育の変革を促進する</h2>
+                        <p className="leading-relaxed mb-8 message-english">Promoting educational reforms
+                by providing open source materials</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import Head from 'next/head';
+// import Head from 'next/head';
 import Header from './components/header';
 import Hero from './components/hero';
+import Philosophy from './components/philosophy';
+import Board from './components/board';
 
 export default function Home() {
     return (
@@ -8,6 +10,8 @@ export default function Home() {
             <Header />
             <main>
                 <Hero />
+                <Philosophy />
+                <Board />
             </main>
         </>
     )
