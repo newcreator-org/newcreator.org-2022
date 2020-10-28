fb720f9bbcf4b5bbfd4a6ebffa6dda49f66fc4d1
yosh1 <yoppy1125yoppy@gmail.com>
2020-10-28 12:10:49 +0900

    Add sponser

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Header from './components/header';
 import Hero from './components/hero';
 import Philosophy from './components/philosophy';
 import Board from './components/board';
+import Sponser from './components/sponser';
 import Footer from './components/footer';
 import Font from './components/font';
 
@@ -14,6 +15,7 @@ export default function Home() {
                 <Hero />
                 <Philosophy />
                 <Board />
+                <Sponser />
                 <Font />
             </main>
             <Footer />
