1fbd2db2819cafaff8f5a38e6c374b2120f35425
yosh1 <yoppy1125yoppy@gmail.com>
2022-03-03 10:27:53 +0900

    Fix relate-link

--- pages/information/[id].js
+++ pages/information/[id].js
@@ -4,121 +4,125 @@ import Footer from "../../components/footer";
 import dayjs from "dayjs";
 import blogStyle from "../../styles/pages/blog.module.scss";
 import {
-    FacebookShareButton,
-    FacebookIcon,
-    LineShareButton,
-    LineIcon,
-    TwitterShareButton,
-    TwitterIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  LineShareButton,
+  LineIcon,
+  TwitterShareButton,
+  TwitterIcon,
 } from "react-share";
 
-export default function BlogId({ blog }) {
-    return (
-        <>
-            <Heads child={blog.title} />
-            <body>
-                <Header />
-                <main>
-                    <section className={blogStyle.header}>
-                        <div>
-                            <time
-                                dateTime={dayjs(blog.publishedAt).format(
-                                    "YYYY-MM-DD"
-                                )}
-                            >
-                                {dayjs(blog.publishedAt).format("YYYY.MM.DD")}
-                            </time>
-                            <h1>{blog.title}</h1>
-                        </div>
-                    </section>
-                    <section className={blogStyle.content}>
-                        <div
-                            dangerouslySetInnerHTML={{
-                                __html: blog.content
-                                    ? `${blog.content}`
-                                    : "本文はありません",
-                            }}
-                        />
+export default function BlogId ({ blog }) {
+  return (
+    <>
+      <Heads child={blog.title} />
+      <body>
+        <Header />
+        <main>
+          <section className={blogStyle.header}>
+            <div>
+              <time
+                dateTime={dayjs(blog.publishedAt).format(
+                  "YYYY-MM-DD"
+                )}
+              >
+                {dayjs(blog.publishedAt).format("YYYY.MM.DD")}
+              </time>
+              <h1>{blog.title}</h1>
+            </div>
+          </section>
+          <section className={blogStyle.content}>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: blog.content
+                  ? `${blog.content}`
+                  : "本文はありません",
+              }}
+            />
 
-                        <p className={blogStyle.link}>
-                            関連リンク:
-                            <a href={blog.link}> {blog.link}</a>
-                        </p>
-                        <div className={blogStyle.sns}>
-                            <p>Share: </p>
-                            <FacebookShareButton
-                                url={[
-                                    `https://newcreator.org/information/${blog.id}`,
-                                ]}
-                                quote={[
-                                    `特定非営利活動法人ニュークリエイター・オルグ｜${blog.title}`,
-                                ]}
-                            >
-                                <FacebookIcon size={28} round />
-                            </FacebookShareButton>
-                            <TwitterShareButton
-                                url={[
-                                    `https://newcreator.org/information/${blog.id}`,
-                                ]}
-                                title={[
-                                    `特定非営利活動法人ニュークリエイター・オルグ｜${blog.title}`,
-                                ]}
-                                via={"nC_org"}
-                            >
-                                <TwitterIcon size={28} round />
-                            </TwitterShareButton>
-                            <LineShareButton
-                                url={[
-                                    `https://newcreator.org/information/${blog.id}`,
-                                ]}
-                                quote={[
-                                    `特定非営利活動法人ニュークリエイター・オルグ｜${blog.title}`,
-                                ]}
-                            >
-                                <LineIcon size={28} round />
-                            </LineShareButton>
-                        </div>
-                        <a href="/">
-                            <p className={blogStyle.top}>TOPに戻る</p>
-                        </a>
-                    </section>
-                </main>
-                <Footer />
-            </body>
-        </>
-    );
+            {
+              blog.link && (
+                <p className={blogStyle.link}>
+                  関連リンク:
+                  <a href={blog.link}> {blog.link}</a>
+                </p>
+              )
+            }
+            <div className={blogStyle.sns}>
+              <p>Share: </p>
+              <FacebookShareButton
+                url={[
+                  `https://newcreator.org/information/${blog.id}`,
+                ]}
+                quote={[
+                  `特定非営利活動法人ニュークリエイター・オルグ｜${blog.title}`,
+                ]}
+              >
+                <FacebookIcon size={28} round />
+              </FacebookShareButton>
+              <TwitterShareButton
+                url={[
+                  `https://newcreator.org/information/${blog.id}`,
+                ]}
+                title={[
+                  `特定非営利活動法人ニュークリエイター・オルグ｜${blog.title}`,
+                ]}
+                via={"nC_org"}
+              >
+                <TwitterIcon size={28} round />
+              </TwitterShareButton>
+              <LineShareButton
+                url={[
+                  `https://newcreator.org/information/${blog.id}`,
+                ]}
+                quote={[
+                  `特定非営利活動法人ニュークリエイター・オルグ｜${blog.title}`,
+                ]}
+              >
+                <LineIcon size={28} round />
+              </LineShareButton>
+            </div>
+            <a href="/">
+              <p className={blogStyle.top}>TOPに戻る</p>
+            </a>
+          </section>
+        </main>
+        <Footer />
+      </body>
+    </>
+  );
 }
 
 // 静的生成のためのパスを指定します
 export const getStaticPaths = async () => {
-    const key = {
-        headers: { "X-API-KEY": process.env.API_KEY },
-    };
-    const data = await fetch(
-        "https://newcreator-org.microcms.io/api/v1/news",
-        key
-    )
-        .then((res) => res.json())
-        .catch(() => null);
-    const paths = data.contents.map((content) => `/information/${content.id}`);
-    return { paths, fallback: false };
+  const key = {
+    headers: { "X-API-KEY": process.env.API_KEY },
+  };
+  const data = await fetch(
+    "https://newcreator-org.microcms.io/api/v1/news",
+    key
+  )
+    .then((res) => res.json())
+    .catch(() => null);
+  const paths = data.contents.map((content) => `/information/${content.id}`);
+  return { paths, fallback: false };
 };
 
 // データをテンプレートに受け渡す部分の処理を記述します
 export const getStaticProps = async (context) => {
-    const id = context.params.id;
-    const key = {
-        headers: { "X-API-KEY": process.env.API_KEY },
-    };
-    const data = await fetch(
-        "https://newcreator-org.microcms.io/api/v1/news/" + id,
-        key
-    )
-        .then((res) => res.json())
-        .catch(() => null);
-    return {
-        props: {
-            blog: data,
-        },
-    };
+  const id = context.params.id;
+  const key = {
+    headers: { "X-API-KEY": process.env.API_KEY },
+  };
+  const data = await fetch(
+    "https://newcreator-org.microcms.io/api/v1/news/" + id,
+    key
+  )
+    .then((res) => res.json())
+    .catch(() => null);
+  return {
+    props: {
+      blog: data,
+    },
+  };
 };
