59bb246652342fee542ebec1efcc1b45bff9b3a7
yosh1 <yoppy1125yoppy@gmail.com>
2023-02-17 11:38:43 +0900

    Fix address

--- pages/index.tsx
+++ pages/index.tsx
@@ -487,12 +487,13 @@ export default function Home({ news }) {
                     </span>
                   </div>
                   <div className="lg:px-20 px-2 flex border-t border-b mb-6 border-gray-200 py-4">
-                    <span className="text-gray-500">東京・五反田事業所</span>
+                    <span className="text-gray-500">兵庫事業所</span>
                     <span className="ml-auto text-gray-900">
-                      〒141-0031<br/>東京都品川区西五反田7-22 TOCビル12F-51
+                      〒671-1523<br/>
+                      兵庫県揖保郡太子町東南 235-1
                     </span>
                   </div>
-                    <small>※ 郵便物等は東京・五反田事業所へお願いいたします。</small>
+                    <small>※ 郵便物等は兵庫事業所へお願いいたします。</small>
                 </div>
               </div>
             </section>
